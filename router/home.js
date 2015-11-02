fadfe913fe5829d21d7ed39beaf3fb08e9b0aa3d
kunl <le720@outlook.com>
2015-11-03 01:47:52 +0800

    更新首页显示内容，添加 author，updateDate 信息

--- router/home.js
+++ router/home.js
@@ -1,5 +1,5 @@
 import Post from '../models/post';
-
+import dateformat from 'dateformat';
 
 export default (req, res) => {
 
@@ -11,6 +11,9 @@ export default (req, res) => {
 
     Post.getTen().then((result) => {
 
+        result.forEach(function(post){
+            post.updateDate = dateformat(post.updateDate, 'yyyy年mm月dd日 HH:MM:ss');
+        })
         res.render('home', {
             title: '主页',
             posts: result
