d67adf3ec67a375b15043ebeed8ebc798aa31db8
kunl <le720@outlook.com>
2015-11-04 02:38:53 +0800

    优化样式

--- router/post_detail.js
+++ router/post_detail.js
@@ -1,13 +1,17 @@
 import Post from '../models/post';
+import dateformat from 'dateformat';
 
 export default (req, res, next) => {
     let title = req.params.title;
     console.log(req.params)
     Post.getPostByTitle(title).then((post) =>{
         if(post){
+            post.createDate = dateformat(post.createDate, 'yyyy年mm月dd日');
+            post.updateDate = dateformat(post.updateDate, 'yyyy年mm月dd日 HH:MM:ss');
             res.render('post', {
                 title: post.title,
-                post: post
+                post: post,
+                desc: post.content.substr(0, 50)
             })
         }else{
             next()
