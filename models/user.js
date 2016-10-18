5645e3a4bc01e826e649a41f0ff25aa18acfe45e
kun l <kun l>
2016-10-18 15:34:44 +0800

    user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 "use strict";
 var _this = this;
-var mongoose_1 = require("mongoose");
-var db_1 = require("../db");
+var mongoose_1 = require('mongoose');
+var mongoose = require('mongoose');
 var _schema = new mongoose_1.Schema({
     id: String,
     created: Date,
@@ -10,37 +10,8 @@ var _schema = new mongoose_1.Schema({
     age: Number
 })
     .pre('save', function (next) {
+    console.log(_this);
     _this.updated = new Date;
     next();
 });
-var _model = db_1["default"].model('Users', _schema);
-var User = (function () {
-    function User() {
-    }
-    User.findById = function (id) {
-        return new Promise(function (resolve, reject) {
-            _model.findById(id).exec().then(function (u) { return u; });
-        });
-    };
-    User.create = function (user) {
-        return new Promise(function (resolve, reject) {
-            _model.create(user).then(function (u) { return u; });
-        });
-    };
-    return User;
-}());
-exports.User = User;
-// export default class {
-//     static get(user){
-//         return new Promise((resolve, reject) => {
-//             pool.createConnection((err, connection) => {
-//                 connection.query('SELECT ?? FROM user where user = ?', [['user', 'passwd', 'nickName'], user], (err, result) => {
-//                     if(err){
-//                         reject(err);
-//                     }
-//                     resolve(result[0]);
-//                 })
-//             })
-//         });
-//     }
-// }
+var User = mongoose.model('Users', _schema);
--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,5 @@
 import { Document, Schema } from 'mongoose';
-import db from '../db';
+import * as mongoose from 'mongoose';
 
 
 export interface IUser extends Document{
@@ -9,9 +9,8 @@ export interface IUser extends Document{
     name: string;
     age: number
 }
-export type  IUserModel = IUser & Document
 
-let _schema:Schema = new Schema({
+let _schema = new Schema({
     id: String,
     created: Date,
     updated: Date,
@@ -19,42 +18,9 @@ let _schema:Schema = new Schema({
     age: Number
 })
 .pre('save', (next) => {
+    console.log(this)
     this.updated = new Date;
     next();
 })
 
-let _model = db.model<IUser>('Users', _schema);
-
-export class User {
-    
-    static findById(id: string): Promise <IUserModel>{
-        return new Promise <IUser>((resolve, reject) => {
-            _model.findById(id).exec().then(u => u)
-        })
-    }
-    static create(user:any) {
-        return new Promise <IUserModel> ((resolve, reject) => {
-            _model.create(user).then(u => u);
-        })
-    }
-
-
-    
-}
-
-
-// export default class {
-//     static get(user){
-//         return new Promise((resolve, reject) => {
-//             pool.createConnection((err, connection) => {
-//                 connection.query('SELECT ?? FROM user where user = ?', [['user', 'passwd', 'nickName'], user], (err, result) => {
-//                     if(err){
-//                         reject(err);
-//                     }
-//                     resolve(result[0]);
-//                 })
-//             })
-            
-//         });
-//     }
-// }
+let User = mongoose.model<IUser>('Users', _schema);
