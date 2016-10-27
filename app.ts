9f2d023e96069f90739b6465c926ca730177e224
kunl <likunn@chanjet.com>
2016-10-27 16:17:21 +0800

    index template

--- app.ts
+++ app.ts
@@ -17,8 +17,6 @@ let app = <express.Application>express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
