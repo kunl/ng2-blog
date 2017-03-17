a95a5be51edbca47d5ca531e8f1725ad46ec82d8
kunl <likunn@chanjet.com>
2017-03-17 13:16:17 +0800

    样式

--- typings.d.ts
+++ typings.d.ts
@@ -1,13 +1,1 @@
-
-export {Post}
-declare class Post {
-    //标题
-    title: string 
-    content: string
-    author: string
-    createDate: Date
-    updateDate?: Date
-
-}
-
 export declare let webpackDevMiddleware: any;
